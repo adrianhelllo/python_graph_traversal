08fc303b998b803fbb318357863deb51258cf99d
adrianhelllo <hetessyadrian@gmail.com>
2025-11-07 11:18:21 +0100

    Version 2: Implemented DFS for weighted directional graph

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class Graph:
     def __init__(self, size):
-        self.adjacency_matrix = [[0] * size for _ in range(size)]
+        self.adjacency_matrix = [[None] * size for _ in range(size)]
         self.vertex_data = [''] * size
         self.size = size
 
@@ -8,28 +8,42 @@ class Graph:
         if 0 <= vertex < self.size:
             self.vertex_data[vertex] = data
 
-    def add_edge(self, f, t):
+    def add_edge(self, f, t, weight):
         if 0 <= f < self.size and 0 <= t < self.size:
-            self.adjacency_matrix[f][t] = 1
-            self.adjacency_matrix[t][f] = 1 #undirectional graph
+            self.adjacency_matrix[f][t] = weight
 
     def print_graph(self):
         print("Adj. Matrix:")
         for row in self.adjacency_matrix:
-            print(' '.join(map(str, row)))
+            print(' '.join(map(lambda x: str(x) if x is not None else '-', row)))
         print("Vertex data:")
         for vertex, data in enumerate(self.vertex_data):
             print(f"Vertex {vertex}: {data}")
+
+    def dfs_util(self, v, visited):
+        visited[v] = True
+        print(self.vertex_data[v], end=' ')
+
+        for i in range(self.size):
+            if self.adjacency_matrix[v][i] is not None and not visited[i]:
+                self.dfs_util(i, visited)
+
+    def dfs(self, start_vertex_data):
+        visited = [False] * self.size
+        start_vertex = self.vertex_data.index(start_vertex_data)
+        self.dfs_util(start_vertex, visited)
 
 g = Graph(4)
 g.add_vertex_data(0, 'A')
 g.add_vertex_data(1, 'B')
 g.add_vertex_data(2, 'C')
 g.add_vertex_data(3, 'D')
-g.add_edge(0, 1)
-g.add_edge(0, 3)
-g.add_edge(1, 3)
-g.add_edge(1, 2)
-g.add_edge(2, 3)
+g.add_edge(0, 1, 0)
+g.add_edge(0, 3, 1)
+g.add_edge(1, 0, 2)
+g.add_edge(1, 2, 3)
+g.add_edge(2, 3, 4)
 
 g.print_graph()
+
+g.dfs('C')
